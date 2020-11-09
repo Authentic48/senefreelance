06e778cdd441e9287575bf0c6e4282f3e395088d
Authentic48 <iboufall48@gmail.com>
2020-11-09 23:02:58 +0700

    GA added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -69,7 +69,7 @@
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
         })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
       
-        ga('create', '252054001', 'auto');
+        ga('create', 'G-3Y6XNWQ74F', 'auto');
         ga('send', 'pageview');
       
     </script>
